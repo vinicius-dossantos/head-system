96ecc25337b419119bff06baed335f96f2ad5cf0
Vinicius Henrique dos Santos <vinicius-dossantos@outlook.com>
2025-05-29 15:33:21 -0300

    Add new data files and update .gitignore to exclude additional file types

--- smart-prop/setup/install_python.bat
+++ smart-prop/setup/install_python.bat
@@ -1,13 +1,21 @@
 @echo off
-echo Downloading Python 3.10.11
+echo Downloading Python 3.10.11...
 powershell -Command "Invoke-WebRequest -Uri 'https://www.python.org/ftp/python/3.10.11/python-3.10.11-amd64.exe' -OutFile $env:TEMP\python-installer.exe"
 
-echo Installing Python 3.10.11
-powershell -Command "Start-Process -FilePath $env:TEMP\python-installer.exe -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait"
+echo Installing Python 3.10.11 silently...
+powershell -Command "Start-Process -FilePath $env:TEMP\python-installer.exe -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1 Include_pip=1' -Wait"
 
-echo Python has been installed successfully.
+echo Verifying Python installation...
 python --version
 
-echo It is done
+echo Verifying pip installation...
+pip --version
+if errorlevel 1 (
+    echo Pip not found. Installing pip manually...
+    python -m ensurepip --upgrade
+    python -m pip install --upgrade pip
+)
+
+echo Installation completed.
 timeout /t 15 >nul
 exit
--- smart-prop/setup/install_vscode.bat
+++ smart-prop/setup/install_vscode.bat
@@ -1,8 +1,7 @@
 @echo off
-cd /d "%~dp0"  :: Muda para o diretório onde o .bat está localizado
-
 echo Downloading the Visual Studio Code installer...
-powershell -Command "Invoke-WebRequest -Uri 'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user' -OutFile 'vscode-installer.exe'"
+
+powershell -Command "Invoke-WebRequest -Uri https://code.visualstudio.com/sha/download?build=stable&os=win32-x64-user -OutFile vscode-installer.exe"
 
 echo Installing VS Code in silent mode...
 vscode-installer.exe /VERYSILENT /NORESTART
@@ -11,5 +10,5 @@ echo Cleaning up the installer...
 del vscode-installer.exe
 
 echo Installation completed. VS Code is ready to use.
-timeout /t 5
+timeout /t 15
 exit
